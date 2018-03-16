65a5fd97916e9154475b0d80306776f28e539c49
Lee Offir <ljotyd@mail.missouri.edu>
2018-03-16 09:03:43 -0500

    First Commit

---
+++ Colors/ColorsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ColorsViewController.swift
+//  Colors
+//
+//  Created by Lee Offir on 3/16/18.
+//  Copyright © 2018 Lee Offir. All rights reserved.
+//
+
+import UIKit
+
+class ColorsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var colorsTableArray = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "B  rown"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colorsTableArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellOfColors", for: indexPath)
+        
+        cell.textLabel?.text = colorsTableArray[indexPath.row]
+        
+        return cell
+}
+}
