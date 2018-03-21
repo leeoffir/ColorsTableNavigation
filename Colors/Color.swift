e7ccc7c95f74eb428a4bcfab2d044fd0c5956d88
Lee Offir <ljotyd@mail.missouri.edu>
2018-03-21 08:23:03 -0500

    Added actual colors after due date for navagation project

---
+++ Colors/Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Lee Offir on 3/21/18.
+//  Copyright © 2018 Lee Offir. All rights reserved.
+//
+
+import UIKit
+
+struct Color
+{
+    let name: String
+    let uiColor: UIColor
+}
+
+
--- Colors/ColorsViewController.swift
+++ Colors/ColorsViewController.swift
@@ -10,17 +10,26 @@ import UIKit
 
 class ColorsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var colorsTableArray = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Brown"]
+   // var colorsTableArray = ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Brown"]
+    
+    var colorsTableArray =
+        [Color(name:"red", uiColor: UIColor.red),
+        Color(name:"orange", uiColor: UIColor.orange),
+        Color(name:"yellow", uiColor: UIColor.yellow),
+        Color(name:"green", uiColor: UIColor.green),
+        Color(name:"blue", uiColor: UIColor.blue),
+        Color(name:"purple", uiColor: UIColor.purple),
+        Color(name:"brown", uiColor: UIColor.brown)]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+       
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -34,8 +43,10 @@ class ColorsViewController: UIViewController, UITableViewDelegate, UITableViewDa
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellOfColors", for: indexPath)
         
-        cell.textLabel?.text = colorsTableArray[indexPath.row]
+        let color = colorsTableArray[indexPath.row]
         
+        cell.textLabel?.text = color.name
+        cell.backgroundColor = color.uiColor
         return cell
 }
 }
